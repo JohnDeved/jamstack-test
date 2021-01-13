7444ac9410d652ef28c99d4c08ad84e0e8ddf5e8
Johann <j.berger@alpin11.at>
2021-01-13 13:17:48 +0100

    + create pages slugs with graphql

--- gatsby-test/gatsby-node.ts
+++ gatsby-test/gatsby-node.ts
@@ -2,18 +2,23 @@ import { GatsbyNode } from 'gatsby'
 import fetch from 'node-fetch'
 import { IPost } from './src/types/common'
 
-// query MyQuery {
-//   allLinks {
-//     group(field: language) {
-//       nodes {
-//         language
-//         url
-//       }
-//     }
-//   }
-// }
+export const createPages: GatsbyNode['createPages'] = async ({ actions: { createPage }, graphql }) => {
+  // use this to replace fetch soon...
+  const result = await graphql(`
+    query {
+      allLinks {
+        group(field: language) {
+          nodes {
+            language
+            url
+          }
+        }
+      }
+    }
+  `)
 
-export const createPages: GatsbyNode['createPages'] = async ({ actions: { createPage } }) => {
+  console.log(result)
+
   const posts = await fetch('http://localhost:3003/posts')
     .then<IPost[]>(data => data.json())
 
